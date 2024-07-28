e8becafaf7b3064d2a04eec95f661bd8ecc6849a
Leeminki319 <minki@ainewt.ai>
2024-07-28 09:35:41 +0900

    Gemini 음성 연결, 길찾기 중 첫번 째 만 네비게이션

--- main.py
+++ main.py
@@ -1,19 +1,51 @@
-from nevigation import *
 import time
 
 from data.walk import walk
 
+from source import *
+
+# Init
+audio = Audio()
+gemini = Gemini()
+navi = Navigation()
+
+output_wav = "data/output.wav"
+output_mp3 = "data/output.mp3"
 
 def main():
-    start = "서울 종로구 사직로8길 31 서울경찰청"
-    end = "서울 중구 세종대로 40"
 
-    print("1. 출발지, 도착지 좌표 가져오기")
-    navi = Navigation(start, end)
-    walking = 0
-
+    print("1. 음성으로 출발지 설정")
     while True:
-        print("2. GPS 좌표 가져오기")
+        # audio.recorde(output_wav, output_mp3)
+        # start = gemini.audio(output_mp3)
+        start = "서울 종로구 사직로8길 31 서울경찰청"
+        start_loc = navi.get_location(start)
+        if not start:
+            print("다시 검색해주세요")
+            continue
+        else:
+            print(f"  - {start}")
+            break
+
+    print("2. 음성으로 도착지 설정")
+    while True:
+        # audio.recorde(output_wav, output_mp3)
+        # end = gemini.audio(output_mp3)
+        end = "서울 중구 세종대로 40"
+        end_loc = navi.get_location(end)
+        if not start:
+            print("다시 검색해주세요")
+            continue
+        else:
+            print(f"  - {end}")
+            break
+
+    print("3. 출발지, 도착지 좌표 가져오기")
+    navi.get_optimal_route(start_loc, end_loc)
+
+    walking = 0
+    print("4. GPS 좌표 가져오기")
+    while True:
         ## 현재 위치 받아 오기
         # my_location = get_gps() # Real location
         my_location = walk[walking] # Test location
---
+++ source/gemini.py
@@ -0,0 +1,46 @@
+import google.generativeai as genai
+import PIL.Image
+
+GOOGLE_API_KEY="AIzaSyBUviRQ_hIoFSLKx-K6opZf_CueU9wuNW0"
+genai.configure(api_key=GOOGLE_API_KEY)
+model = genai.GenerativeModel('models/gemini-1.5-pro')
+
+class Gemini:
+
+    def text(self, text):
+        response = model.generate_content(text)
+
+        print(response.text)
+
+    def audio(self, audio_path):
+        prompt = "말하는 내용 그대로 알려줘"
+
+        audio_file = genai.upload_file(path=audio_path)
+        response = model.generate_content([prompt, audio_file])
+
+        return response.text
+
+    def image(self, path):
+        prompt = "버스의 번호를 알려줘"
+        sample_file = genai.upload_file(path="data/bus.jpeg",
+                                display_name="Jetpack drawing")
+        response = model.generate_content([sample_file, prompt])
+
+        print(response.text)
+
+
+# GOOGLE_API_KEY="AIzaSyBUviRQ_hIoFSLKx-K6opZf_CueU9wuNW0"
+# genai.configure(api_key=GOOGLE_API_KEY)
+# model = genai.GenerativeModel('models/gemini-1.5-pro')
+# text = "넌 누구야"
+# response = model.generate_content(text)
+
+# print(response.text)
+# gemini_audio('asd')
+# gemini_image('asd')
+# text = '''
+# 출발지 : 126.8463,37.313393
+# 도착지 : 126.838745,37.316147
+# '''
+# gemini_text(text + "내가 보내준 위경도로 길 안내 해 줄수 있어?")
+
